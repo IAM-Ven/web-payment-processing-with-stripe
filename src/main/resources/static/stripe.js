00d8b27e116bbca990802348ea4f9a5bd3e9529e
Anas Banun <anas.banun@anasbanun.com>
2017-09-28 14:13:22 +0100

    Adding Broken Down Fields

--- src/main/resources/static/stripe.js
+++ src/main/resources/static/stripe.js
@@ -23,24 +23,80 @@ var style = {
   }
 };
 
-// Create an instance of the card Element
-var card = elements.create('card', {style: style});
+//// Create an instance of the card Element
+//var card = elements.create('card', {style: style});
+//
+//// Add an instance of the card Element into the `card-element` <div>
+//card.mount('#card-element');
+//
+//// Handle real-time validation errors from the card Element.
+//card.addEventListener('change', function(event) {
+//  var displayError = document.getElementById('card-errors');
+//  if (event.error) {
+//    displayError.textContent = event.error.message;
+//  } else {
+//    displayError.textContent = '';
+//  }
+//});
 
-// Add an instance of the card Element into the `card-element` <div>
-card.mount('#card-element');
+//Create an instance of the card number
+var cardNumber = elements.create('cardNumber', {style: style});
 
-// Handle real-time validation errors from the card Element.
-card.addEventListener('change', function(event) {
-  var displayError = document.getElementById('card-errors');
-  if (event.error) {
-    displayError.textContent = event.error.message;
-  } else {
-    displayError.textContent = '';
-  }
+cardNumber.mount('#cardNumber');
+
+cardNumber.addEventListener('change', function(event){
+	var displayError = document.getElementById('cardNumber-errors');
+	if(event.error) {
+		displayError.textContent = event.error.message;
+	} else {
+		displayError.textContent = '';
+	}
+});
+
+//Create an instance of the card Expiry
+var cardExpiry = elements.create('cardExpiry', {style: style});
+
+cardExpiry.mount('#cardExpiry');
+
+cardExpiry.addEventListener('change', function(event){
+	var displayError = document.getElementById('cardExpiry-errors');
+	if(event.error) {
+		displayError.textContent = event.error.message;
+	} else {
+		displayError.textContent = '';
+	}
+});
+
+//Create an instance of the card cvc
+var cardCvc = elements.create('cardCvc', {style: style});
+
+cardCvc.mount('#cardCvc');
+
+cardCvc.addEventListener('change', function(event){
+	var displayError = document.getElementById('cardCvc-errors');
+	if(event.error) {
+		displayError.textContent = event.error.message;
+	} else {
+		displayError.textContent = '';
+	}
+});
+
+//Create an instance of the postal code
+var postalCode = elements.create('postalCode', {style: style});
+
+postalCode.mount('#postalCode');
+
+postalCode.addEventListener('change', function(event){
+	var displayError = document.getElementById('postalCode-errors');
+	if(event.error) {
+		displayError.textContent = event.error.message;
+	} else {
+		displayError.textContent = '';
+	}
 });
 
 // Handle form submission
-var form = document.getElementById('payment-form');
+var form = document.getElementById('payment-form-broken');
 form.addEventListener('submit', function(event) {
   event.preventDefault();
 
@@ -58,7 +114,7 @@ form.addEventListener('submit', function(event) {
 
 function stripeTokenHandler(token) {
 	  // Insert the token ID into the form so it gets submitted to the server
-	  var form = document.getElementById('payment-form');
+	  var form = document.getElementById('payment-form-broken');
 	  var hiddenInput = document.createElement('input');
 	  hiddenInput.setAttribute('type', 'hidden');
 	  hiddenInput.setAttribute('name', 'stripeToken');
