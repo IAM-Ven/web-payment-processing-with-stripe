a120276eabf1acbd644ceb9238d27c60be6a46ad
Anas Banun <anas.banun@anasbanun.com>
2017-09-28 13:19:04 +0100

    Adding Product Table

--- src/main/java/com/stripeappdemo/controllers/StripeController.java
+++ src/main/java/com/stripeappdemo/controllers/StripeController.java
@@ -1,15 +1,35 @@
 package com.stripeappdemo.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.stripeappdemo.models.CartItem;
+import com.stripeappdemo.models.Product;
+import com.stripeappdemo.repository.CartItemRepository;
+import com.stripeappdemo.repository.ProductRepository;
+
 @Controller
 @RequestMapping("/stripe")
 public class StripeController {
+	
+	@Autowired
+	private ProductRepository productRepository;
+	
+	@Autowired
+	private CartItemRepository cartItemRepository;
 
 	@RequestMapping("/")
 	public String stripe (Model model) {
+		List<Product> productList = (List<Product>) productRepository.findAll();
+		List<CartItem> cartItemList = (List<CartItem>) cartItemRepository.findAll();
+		
+		model.addAttribute("productList", productList);
+		model.addAttribute("cartItemList", cartItemList);
+		
 		return "stripe-cart";
 	}
 }
